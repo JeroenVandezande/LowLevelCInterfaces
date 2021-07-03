7ddcf97c950a3af12f3b5fde2a7ae6840c5399fb
Jeroen Vandezande <Jeroen.Vandezande@gmail.com>
2021-07-03 09:04:12 +0200

    added missing include

---
+++ LL_uart.h
@@ -0,0 +1,10 @@
+//
+// Created by Jeroen Vandezande on 03/07/2021.
+//
+
+#ifndef _LL_UART_H_
+#define _LL_UART_H_
+
+typedef void (* TransmitToUARTMethod_t)(uint8_t* data, size_t length);
+
+#endif //_LL_UART_H_
