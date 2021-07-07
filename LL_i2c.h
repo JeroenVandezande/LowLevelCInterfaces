0e0f6c7e2786dd6c58d2fdfc7ad71e24691c172c
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2021-07-07 09:54:27 +0200

    updated "namespaces"

--- LL_i2c.h
+++ LL_i2c.h
@@ -1,16 +1,34 @@
+//MIT License
 //
-// Created by Jeroen Vandezande on 23/06/2021.
+//Copyright (c) 2021 Jeroen Vandezande
 //
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
 
-#ifndef _I2C_H_
-#define _I2C_H_
+#ifndef _LL_I2C_H_
+#define _LL_I2C_H_
 
-typedef bool (*I2CMaster_ReadMethod_t)(uint8_t address, uint8_t * data, size_t length);
-typedef bool (*I2CMaster_WriteMethod_t)(uint8_t address, uint8_t * data, size_t length);
-typedef bool (*I2CMaster_ReadWriteMethod_t)(uint8_t address, uint8_t * data, size_t readLength, size_t writeLength);
+typedef bool (*LL_I2CMaster_ReadMethod_t)(uint8_t address, uint8_t * data, size_t length);
+typedef bool (*LL_I2CMaster_WriteMethod_t)(uint8_t address, uint8_t * data, size_t length);
+typedef bool (*LL_I2CMaster_ReadWriteMethod_t)(uint8_t address, uint8_t * data, size_t readLength, size_t writeLength);
 
-#endif //_I2C_H_
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LowLevelCInterfaces
-A CPM library that contains common type defs to be used to abstract libs to different MCUs
+A CMAKE CPM library that contains common type defs to be used to abstract libs to different MCUs
+
+- a I2C interface header
+- a SPI interface header
+- a UART interface header
